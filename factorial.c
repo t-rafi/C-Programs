81b05dbd21e2206c035e4e1aac6fdc4d816d41e9
t-rafi <towhidulrafi05@gmail.com>
2025-10-06 14:53:14 +0600

    factorial

---
+++ factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int num, i, fact=1;
+printf("Enter number: ");
+scanf("%d", &num);
+for(i=1; i<=num; i++){
+fact= fact*i;
+}
+printf("Factorial of %d is: %d", num, fact);
+
+
+return 0;
+}
