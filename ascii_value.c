40b3f715ca57a0707b771d63dfaab45811be4799
t-rafi <towhidulrafi05@gmail.com>
2025-10-06 11:34:13 +0600

    finding_leap_year.c

--- ascii_value.c
+++ ascii_value.c
@@ -1,1 +1,9 @@
+#include<stdio.h>
+int main(){
+    char ascii;
+    printf("Enter character for ASCII value: ");
+    scanf("%c", &ascii);
+    printf("%c ASCII value is: %d", ascii, ascii);
+    return 0;
+}
 
---
+++ finding_leap_year.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+int year;
+jump:
+printf("Enter year: ");
+scanf("%d",&year);
+if (year % 400==0){
+    printf("Year is leaf year\n\n");
+}
+else if(year%100==0){
+    printf("Not a leap Year \n\n");
+}
+else if(year % 4 == 0){
+    printf("Year is leap year \n\n");
+}
+else{
+    printf("Not a leap year\n\n");
+}
+goto jump;
+return 0;
+}
